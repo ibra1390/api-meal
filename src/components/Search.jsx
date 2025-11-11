c88971af8c0229c6c160797f0ebb50b7c0cdb4b1
Jorge2712 <jorgeps2712@gmail.com>
2025-11-11 15:08:49 -0500

    Uniendo grid con Home

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ export default function BarraBusqueda() {
     return (
         <div className='flex items-center justify-center p-6 gap-1'>
             <input type="text" placeholder='Nombre...' className=' border-2 ' />
-            <button className='flex border-none rounded-2xl bg-blue-600 text-white p-2 hover:bg-blue-500 cursor-pointer'>Buscar</button>
+            <button className='flex border-none rounded-2xl bg-red-500 text-white p-2 hover:bg-red-400 cursor-pointer'>Buscar</button>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,18 @@
-// App.jsx
 import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HeroCarousel from './components/HeroCarousel'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import HeroCarousel from '../components/HeroCarousel'
+import Grid from './Grid.jsx'
 
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
-      <Header />
-      
-      <main className="flex-1">
-        <HeroCarousel />
-        
-        
-      </main>
-      
-      <Footer />
-    </div>
-  )
+export default function Home() {
+    return (
+        <div className="min-h-screen bg-gray-100 flex flex-col">
+            <Header />
+            <main className="flex-1">
+                <HeroCarousel />
+            </main>
+            <Grid />
+            <Footer />
+        </div>
+    )
 }
-
-export default App
