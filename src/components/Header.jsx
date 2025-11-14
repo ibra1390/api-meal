4384e0a3f3fd2af561e22c3d46ea53994d3bf9a7
ibraim <ing.gonzalezvij@hotmail.com>
2025-11-13 19:15:10 -0600

    pagina de ingredientes agregada y menu hamburguesa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import HamburguesaMenu from './HamburguesaMenu'
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  
   return (
     <header className='w-full bg-red-500 text-white'>
       <div className="flex items-center justify-between py-3 px-4">
@@ -18,11 +21,16 @@ export default function Header() {
           <h1 className="text-2xl sm:text-3xl font-bold font-playfair">La Cocinita</h1>
           <p className="text-sm opacity-90 mt-1 hidden sm:block font-inter">Cooking with Passion</p>
         </div>
-        <div className="w-16 md:w-20"></div>
+        {/* Boton Anvorguesa */}
+        <button onClick={() => setOpen(!open)} className="text-2xl p-2 sm:hidden">
+          {open ? '✕' : '☰'}
+        </button>
+
+        <div className="w-16 hidden sm:block"></div>
       </div>
 
       {/* Navigation Bar */}
-      <nav className="bg-red-600 border-t border-red-300">
+      <nav className="bg-red-600 border-t border-red-300 hidden sm:block">
         <div className="max-w-6xl mx-auto px-4">
           <div className="flex justify-center space-x-4 sm:space-x-8 py-2">
             <Link to="/" className="text-white px-3 sm:px-4 py-2 rounded-lg transition-all duration-300 font-inter hover:scale-105">
@@ -34,9 +42,14 @@ export default function Header() {
             <Link to="/countries" className="text-white px-3 sm:px-4 py-2 rounded-lg transition-all duration-300 font-inter hover:scale-105">
               Countries
             </Link>
+            <Link to="/ingredients" className="text-white px-3 sm:px-4 py-2 rounded-lg transition-all duration-300 font-inter hover:scale-105">
+              Ingredients
+            </Link>
           </div>
         </div>
       </nav>
+      {/* Abrir menu en movil */}
+      <HamburguesaMenu open={open} close={setOpen} />
     </header>
   )
 }
